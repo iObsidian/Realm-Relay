c5d46d442792fa55b9a89821c2aafa417d5300ad
iObsidian <rotmgiobsidian@gmail.com>
2014-08-03 20:44:41 -0400

    Updated realm relay to current build
    updated to new build

--- src/realmrelay/PacketManager.java
+++ src/realmrelay/PacketManager.java
@@ -1,17 +1,21 @@
 package realmrelay;
 
+import java.io.IOException;
 import java.util.HashMap;
 import realmrelay.data.Entity;
-import realmrelay.data.Location;
 import realmrelay.data.PlayerData;
+import realmrelay.data.SlotObject;
 import realmrelay.data.StatData;
 import realmrelay.data.Status;
 import realmrelay.packets.Packet;
+import realmrelay.packets.client.EscapePacket;
 import realmrelay.packets.client.HelloPacket;
+import realmrelay.packets.client.PlayerShootPacket;
 import realmrelay.packets.client.PlayerTextPacket;
+import realmrelay.packets.client.UseItemPacket;
+import realmrelay.packets.client.UsePortalPacket;
 import realmrelay.packets.server.Create_SuccessPacket;
 import realmrelay.packets.server.New_TickPacket;
-import realmrelay.packets.server.TextPacket;
 import realmrelay.packets.server.UpdatePacket;
 import realmrelay.script.PacketScriptEvent;
 
@@ -19,12 +23,16 @@ public class PacketManager {
 	
 	public static PlayerData playerData = new PlayerData();
 	
-	private long startTime = 0;
+	private static long startTime = 0;
 	
 	public static HashMap<String, PlayerData> nameAndID = new HashMap<String, PlayerData>();
 	
+	private static Packet lastUseItemPacket;
+	
+	private static boolean canGo;
+	
 	public static void onClientPacketEvent(final PacketScriptEvent event) throws Exception {
-		Packet packet = event.getPacket();
+		final Packet packet = event.getPacket();
 		
 		if (packet instanceof PlayerTextPacket) {
 			PlayerTextPacket ptp = (PlayerTextPacket) packet;
@@ -43,18 +51,39 @@ public class PacketManager {
 				
 			}
 			
+		}
+		if (packet instanceof UsePortalPacket) {
+			UsePortalPacket upk = (UsePortalPacket) packet;
+			System.out.println(upk.objectId);
 		} else if (packet instanceof HelloPacket) {
 			
 			HelloPacket mpk = (HelloPacket) event.getPacket();
 			
 			playerData.id = mpk.gameId;
 			
+		} else if (packet instanceof PlayerShootPacket) {
+			PlayerShootPacket psp = (PlayerShootPacket) event.getPacket();
+			
+			event.cancel();
+			
+			System.out.println("PlayerShot " + canGo);
+			
+			if (canGo == true) {
+				canGo = false;
+				
+				event.sendToServer(event.getPacket());
+				event.sendToServer(event.getPacket());
+			} else {
+				event.cancel();
+				canGo = true;
+			}
+			
 		}
 		
 		return;
 	}
 	
-	public int currentTime() {
+	public static int currentTime() {
 		return (int) (System.currentTimeMillis() - startTime);
 	}
 	
