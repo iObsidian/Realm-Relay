c426826e1200b0e0dfa731a8b58c0f3bd4630ad2
Alde <iObsidian@users.noreply.github.com>
2014-12-10 15:36:33 -0500

    26.0.1

--- src/realmrelay/packets/client/HelloPacket.java
+++ src/realmrelay/packets/client/HelloPacket.java
@@ -5,36 +5,40 @@ import java.io.DataOutput;
 import java.io.IOException;
 import realmrelay.packets.Packet;
 
-/* Thanks to Maat, Gratin and pieoewieoe for their contribution */
+/* Thanks to Maat, Faintmako, Gratin and pieoewieoe for their contribution */
 
 public class HelloPacket extends Packet {
-	
-	public String buildVersion;
+
+	public String buildVersion = "";
 	public int gameId = 0;
-	public String guid;
-	public String password;
-	public String secret;
-	public int keyTime = 0;
+	public String guid = "";
+	public String password = "";
+	public String secret = "";
+	public int keyTime;
 	public byte[] key = new byte[0];
-	public String obf1;
+	public byte[] obf1 = new byte[0];
 	public String obf2 = "";
 	public String obf3 = "";
 	public String obf4 = "";
 	public String obf5 = "";
 	public String obf6 = "";
+	public int obf7 = 0;
 	
 	@Override
 	public void parseFromInput(DataInput in) throws IOException {
 		
 		this.buildVersion = in.readUTF();
+		
 		this.gameId = in.readInt();
 		this.guid = in.readUTF();
+		
 		this.password = in.readUTF();
 		this.secret = in.readUTF();
 		this.keyTime = in.readInt();
 		this.key = new byte[in.readShort()];
 		in.readFully(this.key);
-		this.obf1 = in.readUTF();
+		this.obf1 = new byte[in.readInt()];
+		in.readFully(this.obf1);
 		this.obf2 = in.readUTF();
 		this.obf3 = in.readUTF();
 		this.obf4 = in.readUTF();
@@ -47,18 +51,17 @@ public class HelloPacket extends Packet {
 	public void writeToOutput(DataOutput out) throws IOException {
 		
 		out.writeUTF(this.buildVersion);
+		
 		out.writeInt(this.gameId);
 		out.writeUTF(this.guid);
+		
 		out.writeUTF(this.password);
 		out.writeUTF(this.secret);
 		out.writeInt(this.keyTime);
-		
 		out.writeShort(this.key.length);
 		out.write(this.key);
-		
-		out.writeInt(this.obf1.length());
-		out.writeBytes(this.obf1);
-		
+		out.writeInt(this.obf1.length);
+		out.write(this.obf1);
 		out.writeUTF(this.obf2);
 		out.writeUTF(this.obf3);
 		out.writeUTF(this.obf4);
