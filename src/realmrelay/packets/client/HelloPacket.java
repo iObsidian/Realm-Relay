0f80255347ff1e0bebc9b9e697b39f53045318e7
Alde <iObsidian@users.noreply.github.com>
2014-12-29 22:46:04 -0500

    Updated 27.0.wathever
    Fuck this game

--- src/realmrelay/packets/client/HelloPacket.java
+++ src/realmrelay/packets/client/HelloPacket.java
@@ -5,11 +5,14 @@ import java.io.DataOutput;
 import java.io.IOException;
 import realmrelay.packets.Packet;
 
-/* Thanks to Maat, Faintmako, Gratin and pieoewieoe for their contribution */
+/* Thanks to me, Maat, Faintmako, Gratin and pieoewieoe, for their contribution. */
 
 public class HelloPacket extends Packet {
-
+	
+	public int Random1 = 0;
+	public int Random2 = 0;
 	public String buildVersion = "";
+	//public int obf0;
 	public int gameId = 0;
 	public String guid = "";
 	public String password = "";
@@ -26,13 +29,13 @@ public class HelloPacket extends Packet {
 	
 	@Override
 	public void parseFromInput(DataInput in) throws IOException {
-		
 		this.buildVersion = in.readUTF();
-		
+		//this.obf0 = in.readInt();
 		this.gameId = in.readInt();
 		this.guid = in.readUTF();
-		
+		this.Random1 = in.readInt();
 		this.password = in.readUTF();
+		this.Random2 = in.readInt();
 		this.secret = in.readUTF();
 		this.keyTime = in.readInt();
 		this.key = new byte[in.readShort()];
@@ -44,18 +47,18 @@ public class HelloPacket extends Packet {
 		this.obf4 = in.readUTF();
 		this.obf5 = in.readUTF();
 		this.obf6 = in.readUTF();
-		
+		//this.obf7 = in.readInt();
 	}
 	
 	@Override
 	public void writeToOutput(DataOutput out) throws IOException {
-		
 		out.writeUTF(this.buildVersion);
-		
+		//out.writeInt(this.obf0);
 		out.writeInt(this.gameId);
 		out.writeUTF(this.guid);
-		
+		out.writeInt(Random1);
 		out.writeUTF(this.password);
+		out.writeInt(Random2);
 		out.writeUTF(this.secret);
 		out.writeInt(this.keyTime);
 		out.writeShort(this.key.length);
@@ -67,6 +70,7 @@ public class HelloPacket extends Packet {
 		out.writeUTF(this.obf4);
 		out.writeUTF(this.obf5);
 		out.writeUTF(this.obf6);
-		
+		//out.writeInt(this.obf7);
 	}
+	
 }
