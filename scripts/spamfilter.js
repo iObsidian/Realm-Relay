8deee4c999d0bf482080e5653449b7f6b187f168
Alde <iObsidian@users.noreply.github.com>
2014-12-29 22:49:28 -0500

    New Spamfilter for fucking oryx bots.
    (y)

--- scripts/spamfilter.js
+++ scripts/spamfilter.js
@@ -1,48 +1,35 @@
 // spamfilter.js
 
 var ID_TEXT = $.findPacketId("TEXT");
+var ID_PLAYERTEXT = $.findPacketId("PLAYER_TEXT");
 
 var keywords = [
-	".info",
+	"oryxsh0p",
 	".net",
-	".org",
-	".us",
-	"% off",
-	"24/7",
-	"cheap",
-	"client",
-	"customer",
-	"del|very",
-	"delivery",
-	"dellvery",
-	"pric", // pricing/prices/price
-	"prize",
-	"satis", // satisfaction/satisfactory/satisfied
-	"service",
-	"stock",
-	"www"
 ];
 
 function onServerPacket(event) {
-	// get the packet involved in this event
 	var packet = event.getPacket();
 	
-	// if this event's packet is a TEXT packet...
 	if (packet.id() == ID_TEXT) {
 	
 		// make text lowercase to match the keyword list
 		var text = packet.text.toLowerCase();
-		
+		var name = packet.name.toLowerCase();
+
 		// loop through every keyword for testing
 		for (var i = 0; i < keywords.length; i++) {
 		
 			// if keyword exists in the text...
 			if (text.indexOf(keywords[i]) != -1) {
 				
-				// cancel the event to stop the packet from being sent to the client
+				playerTextPacket = event.createPacket(ID_PLAYERTEXT);
+				playerTextPacket.text = "/ignore "+name;
+				event.sendToServer(playerTextPacket);
+
+				event.echo("Ignored "+name+".");
+					
 				event.cancel();
-				
-				// break the loop because we already know the packet is spam
 				break;
 			}
 		}
